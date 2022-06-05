a18d5a208c8344d18b9703ebe9471b1386f4df78
SergioBalca <3896@holbertonschool.com>
2022-06-04 19:03:33 -0500

    JavaScript file that allows to make a POST request to places_search when the button search is clicked

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -45,4 +45,33 @@ $('document').ready(function () {
       });
     }
   });
+
+  $('.filters > button').click(function () {
+    $('.places > article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        $.each(data, function (i, v) {
+          $(`<article>
+          <div class="title_box">
+            <h2>${v.name}</h2>
+            <div class="price_by_night">$${v.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${v.max_guest} Guest</div>
+                  <div class="number_rooms">${v.number_rooms} Bedroom</div>
+                  <div class="number_bathrooms">${v.number_bathrooms} Bathroom</div>
+          </div>
+                <div class="description">
+            ${v.description}
+                </div>
+        </article>`).appendTo('.places');
+        });
+      }
+    });
+  });
 });
